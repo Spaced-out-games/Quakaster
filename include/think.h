668df4609cee28ecafe6ad8d09b82915974bbda4
Devin Frost <devin.frost4311@gmail.com>
2024-11-29 23:17:12 -0500

    Updated the timing system to use a TPS system rather than a concrete time. Makes it easier to do conversions, since they are kind of already there for you.
    Also it uses microseconds instead of milliseconds for more fine-tuned control

--- include/think.h
+++ include/think.h
@@ -5,20 +5,27 @@
 class Scene;
 
 
+
 // Script to be executed on a set interval in terms of a Scene. 
 class thinkScript
 {
 public:
-    // Constructor
-    thinkScript(func_ptr_t<void, Scene&> func, int ticks_per_second = 600)
+    // Constructor. Defaults to 60 tps by default
+    thinkScript(func_ptr_t<void, Scene&> func, int tps)
         : function(func),
-        tick_interval(std::chrono::milliseconds(1000 / ticks_per_second)),
-        next_tick(std::chrono::high_resolution_clock::now() + tick_interval)
+        tick_interval(std::chrono::microseconds(1000000 / tps)),
+        next_tick(std::chrono::high_resolution_clock::now() + std::chrono::microseconds(tick_interval))
+    {
+    }
+    thinkScript(func_ptr_t<void, Scene&> func)
+        : function(func),
+        tick_interval(std::chrono::microseconds(0)),
+        next_tick(std::chrono::high_resolution_clock::now() + std::chrono::microseconds(tick_interval))
     {
     }
 
-    // Tick interval in milliseconds
-    std::chrono::milliseconds tick_interval;
+    // Tick interval in microseconds
+    std::chrono::microseconds tick_interval;
 
     // time of the next tick
     std::chrono::time_point<std::chrono::high_resolution_clock> next_tick;
@@ -44,14 +51,14 @@ public:
         next_tick += tick_interval;
     }
 
-    // Set the tick interval
-    void setTickInterval(int ms)
+    // Set the tick interval (in microseconds)
+    void setTickInterval(int us)
     {
-        tick_interval = std::chrono::milliseconds(ms);
+        tick_interval = std::chrono::microseconds(us);
     }
     // Manually set the next tick time. This is great if you want to set a unique interval for a single interval then have it revert
-    void advanceTickManual(int ms)
+    void advanceTickManual(int us)
     {
-        next_tick += std::chrono::milliseconds(ms);
+        next_tick += std::chrono::microseconds(us);
     }
 };
