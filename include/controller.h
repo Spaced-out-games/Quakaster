a41fba02e93cc16f765ff00f4c90a9225dc62dff
Morty-Mcfly <frostd4713@students.clarkstate.edu>
2024-12-03 14:58:03 -0500

    Added an SDL_Keycode -> char converter Added a think function for KeyPressEvent Controller now maintains a referene to the application that owns it Fixed some bugs in processInput (Forgot some curlies)

--- include/controller.h
+++ include/controller.h
@@ -1,10 +1,37 @@
-#include <SDL2/SDL.h>
+#include <SDL.h>
 #include <functional>
 #include "scene.h"
+
+char SDLKeyToChar(SDL_Keycode keycode, bool shiftPressed = false) {
+    // Handle alphabetic keys
+    if (keycode >= SDLK_a && keycode <= SDLK_z) {
+        return shiftPressed ? (keycode - SDLK_a + 'A') : (keycode - SDLK_a + 'a');
+    }
+    // Handle numeric keys
+    if (keycode >= SDLK_0 && keycode <= SDLK_9) {
+        return static_cast<char>(keycode);
+    }
+    // Handle space
+    if (keycode == SDLK_SPACE) {
+        return ' ';
+    }
+
+    // Add more mappings as needed
+    return '\0'; // Return null char for unsupported keys
+}
 
 struct KeyPressEvent {
     SDL_Keycode key;
     Uint16 mod;
+    static void print(KeyPressEvent event) {
+        console_log(std::string("Recieved KeyPressEvent"),console_colors::RED);
+        if (event.key == SDLK_ESCAPE)
+        {
+            Application::current_application->console.open ^= 1;
+
+        }
+        //event.target->log(event.color_ID, event.message.c_str());
+    }
 };
 
 struct KeyHoldEvent {
@@ -46,44 +73,54 @@ struct MouseMoveEvent {
 
 class Controller {
 public:
-    entt::dispatcher& dispatcher;
+    Application& app;
 
     // Tracks the last mouse position
     int lastMouseX = 0;
     int lastMouseY = 0;
 
-    explicit Controller(entt::dispatcher& dispatcher)
-        : dispatcher(dispatcher), lastMouseX(0), lastMouseY(0) {}
+    explicit Controller(Application& app)
+        : app(app), lastMouseX(0), lastMouseY(0)
+    {
+        ADD_EVENT_LISTENER(app.scene.dispatcher, KeyPressEvent, KeyPressEvent::print);
+    }
 
     void processInput() {
+        
         SDL_Event event;
 
         while (SDL_PollEvent(&event)) {
+
+            if (app.console.open) {
+                ImGui_ImplSDL2_ProcessEvent(&event);
+                return;
+            }
+            
             switch (event.type) {
             case SDL_KEYDOWN:
                 if (!event.key.repeat) {
                     KeyPressEvent keyPressEvent{ event.key.keysym.sym, event.key.keysym.mod };
-                    dispatcher.trigger(keyPressEvent);
+                    app.scene.dispatcher.trigger(keyPressEvent);
                 }
                 else {
                     KeyHoldEvent keyHoldEvent{ event.key.keysym.sym, event.key.keysym.mod };
-                    dispatcher.trigger(keyHoldEvent);
+                    app.scene.dispatcher.trigger(keyHoldEvent);
                 }
                 break;
 
-            case SDL_KEYUP:
+            case SDL_KEYUP: {
                 KeyReleaseEvent keyReleaseEvent{ event.key.keysym.sym, event.key.keysym.mod };
-                dispatcher.trigger(keyReleaseEvent);
+                app.scene.dispatcher.trigger(keyReleaseEvent);
                 break;
-
+            }
             case SDL_MOUSEBUTTONDOWN: {
                 MouseClickEvent mouseClickEvent{ event.button.button, event.button.x, event.button.y };
-                dispatcher.trigger(mouseClickEvent);
+                app.scene.dispatcher.trigger(mouseClickEvent);
             } break;
 
             case SDL_MOUSEBUTTONUP: {
                 MouseReleaseEvent mouseReleaseEvent{ event.button.button, event.button.x, event.button.y };
-                dispatcher.trigger(mouseReleaseEvent);
+                app.scene.dispatcher.trigger(mouseReleaseEvent);
 
                 if (event.button.clicks > 1) {
                     MouseButtonClickEvent mouseButtonClickEvent{
@@ -91,7 +128,7 @@ public:
                         event.button.x,
                         event.button.y,
                         event.button.clicks };
-                    dispatcher.trigger(mouseButtonClickEvent);
+                    app.scene.dispatcher.trigger(mouseButtonClickEvent);
                 }
             } break;
 
@@ -101,7 +138,7 @@ public:
 
                 MouseMoveEvent mouseMoveEvent{
                     event.motion.x, event.motion.y, deltaX, deltaY };
-                dispatcher.trigger(mouseMoveEvent);
+                app.scene.dispatcher.trigger(mouseMoveEvent);
 
                 // Update last mouse position
                 lastMouseX = event.motion.x;
