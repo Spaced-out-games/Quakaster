4970aae1b8a1acc4aee66a9c9664a8aae30d809d
Morty-Mcfly <frostd4713@students.clarkstate.edu>
2024-12-25 00:34:17 -0500

    Injected ConsoleInterpreter UIContext, and eventHandler dependencies overrode base classes

--- include/GameContext/UI/ConsoleUI.h
+++ include/GameContext/UI/ConsoleUI.h
@@ -4,6 +4,10 @@
 #include <backends/imgui_impl_opengl3.h>
 #include "imgui.h"
 #include <include/GameContext/server/ConsoleInterpreter.h>
+#include "console_message.h"
+#include <include/GameContext/UI/UIContext.h> // Include UIContext before implementing methods
+#include <include/GameContext/IO/InputBase.h>
+#include <include/GameContext/UI/UIBase.h>
 
 // Size of a single console command
 #define CONSOLE_BUFFER_SIZE 256
@@ -11,26 +15,22 @@
 // Which character is used to accent user input within the integrated console
 #define CONSOLE_ACCENT_CHAR '~'
 
-
-#include "console_message.h"
-
-
-
 struct UIContext;
 
-struct ConsoleUI
+struct ConsoleUI: public InputBase, public UIBase
 {
-    bool is_active(UIContext& context);
-    ConsoleInterpreter interpreter;
+    ConsoleInterpreter& interpreter;
     eventHandler& event_handler;
-    ConsoleUI(ConsoleInterpreter& interpreter, eventHandler& event_handler) :
+    UIContext& ui_context;
+    ConsoleUI(ConsoleInterpreter& interpreter, eventHandler& event_handler, UIContext& ui_context) :
         interpreter(interpreter),
-        event_handler(event_handler) {}
+        event_handler(event_handler),
+        ui_context(ui_context) {}
+    void draw() override {}
+    void on_event(const SDL_Event& event) override
+    {
+        ImGui_ImplSDL2_ProcessEvent(&event);
+    }
 };
 
-#include <include/GameContext/UI/UIContext.h>
 
-bool ConsoleUI::is_active(UIContext& context)
-{
-    return context.
-}
