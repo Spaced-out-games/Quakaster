aa659c86b1571e66d9e58928f9dc127a70dddcb9
Devin Frost <devin.frost4311@gmail.com>
2025-01-09 02:15:36 -0500

    Added some wothless comments

--- include/GameContext/utils/utility_types.h
+++ include/GameContext/utils/utility_types.h
@@ -1,6 +1,16 @@
 #pragma once
 
+/*
+#include <thirdparty/glm/glm.hpp>
+#include <thirdparty/glm/gtc/quaternion.hpp>
+#include <thirdparty/glm/gtc/matrix_transform.hpp>
+#include <thirdparty/glm/gtc/type_ptr.hpp>
+*/
+
 
 // Templated function pointer. Easy to read and understand, lol
 template <typename return_t, typename... arg_t>
 using func_ptr_t = return_t(*)(arg_t...);
+
+
+
