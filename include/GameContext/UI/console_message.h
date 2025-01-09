2ce874ccba021e0aa1c8efa8bc2184b7f3bf883c
Devin Frost <devin.frost4311@gmail.com>
2025-01-09 02:14:38 -0500

    Moved console_color into its own file Added a constructor for console_message

--- include/GameContext/UI/console_message.h
+++ include/GameContext/UI/console_message.h
@@ -1,26 +1,17 @@
 #pragma once
 #include <string>
-enum class console_color : uint8_t
-{
-    WHITE = 0,
-    DEFAULT_TEXT = WHITE,
-    RED = 1,
-    DEFAULT_ERROR_CRITICAL = RED,
-    GREEN = 2,
-    DEFAULT_OKAY = GREEN,
-    YELLOW = 3,
-    DEFAULT_WARNING = YELLOW,
-    ORANGE = 4,
-    DEFAULT_WARNING_SEVERE = ORANGE
-};
+#include "console_color.h"
+
 
 struct console_message {
     console_color color;
     std::string message;
-    console_message(std::string message, console_color color = console_color::DEFAULT_TEXT) : color(color), message(message) {}
 
-    console_message& operator+=(std::string append_string)
-    {
+    console_message(std::string message, console_color color = console_color::DEFAULT_TEXT)
+        : color(color), message(message) {}
+
+    console_message& operator+=(const std::string& append_string) {
         message += append_string;
+        return *this; // Return the modified object
     }
 };
