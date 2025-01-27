30da18b60409c20434e2771026dafd8bf945ad1f
Devin Frost <devin.frost4311@gmail.com>
2025-01-26 23:23:43 -0500

    Added documentation

--- include/GameContext/client/application.h
+++ include/GameContext/client/application.h
@@ -3,13 +3,18 @@
 #include <include/GameContext/UI/UIContext.h>
 #include <include/GameContext/IO/InputDelegate.h>
 
+// Basic application structure. Contains a window and a UIContext (built on ImGUI)
 struct Application
 {
 	
 	Window window;
+	
+	// Contains all the UI widgets on the screen and draws them all using draw().
 	UIContext ui_context;
 
+	// TODO: Remove the ConsoleInterpreter dependency; remove the automatic console construction to meet this requirement; add some methods for getting the first instance, using static_cast
 
+	// Constructor, takes in a entt::dispatcher& for dispatching events, and a consoleInterpreter& for telling the built-in console where to send its commands after tokenization
 	Application(eventHandler& event_handler, ConsoleInterpreter& interpreter) : ui_context( event_handler, interpreter, window.get_renderer(), window), window(interpreter)
 	{
 	}
