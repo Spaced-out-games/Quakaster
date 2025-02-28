f5322adbfee0c9c710caf092ddfb6f592417a844
Devin Frost <devin.frost4311@gmail.com>
2025-02-28 16:18:12 -0500

    Initial commit

---
+++ include/common.h
@@ -0,0 +1,16 @@
+#pragma once
+// Include all the base 
+#include <include/GameContext/base/Quakaster.h>
+
+/// Basically an IDE hack. Allows you to basically wrap and name scopes within VS Code, my editor of choice.
+/// It is nothing more than a no-op but tricks Intellisense into thinking it's a funciton when it's not
+#define SECTION(x)
+
+// Minimal C-style function pointer type
+template <typename return_type, typename... argument_types>
+using func_ptr_t = return_type(*)(argument_types...);
+
+
+
+
+
