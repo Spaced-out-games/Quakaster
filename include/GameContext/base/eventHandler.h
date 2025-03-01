ba535309ca448a33fb0f693ac54ce2e53a953731
Devin Frost <devin.frost4311@gmail.com>
2025-03-01 17:20:16 -0500

    Added EventHandler, a wrapper for entt::dispatcher

--- include/GameContext/base/eventHandler.h
+++ include/GameContext/base/eventHandler.h
@@ -1,5 +1,14 @@
 #pragma once
 #include <include/thirdparty/entt.hpp>
 
+namespace Quakaster::base {
 
-struct eventHandler : public entt::dispatcher {};
+
+	struct EventHandler : public entt::dispatcher {
+		// Nothing special
+	};
+
+
+}
+
+using EventHandler = Quakaster::base::EventHandler;
