02c318b2d6413516cd3f99898aca800c6df09a37
Devin Frost <devin.frost4311@gmail.com>
2025-03-01 17:26:34 -0500

    Moved most functionality ino an ISystem for easier insertion of game logic
    No longer takes a Camera& to draw; this is found via Camera::target_camera.
    Yes this couples shit, but I need a consistent interface (ISystem)

--- include/GameContext/components/AABB.h
+++ include/GameContext/components/AABB.h
@@ -24,64 +24,88 @@ namespace Quakaster::components {
 		glm::vec3& location;
 		glm::vec3 dimensions = { 1.5, 3.0, 1.5 };
 
-		static void init()
-		{
-			std::vector<default_vertex_t> vertices = {
-				{{-0.5f, -0.5f, -0.5f}},  // Vertex 0: Bottom-left-back
-				{{ 0.5f, -0.5f, -0.5f}},  // Vertex 1: Bottom-right-back
-				{{ 0.5f,  0.5f, -0.5f}},  // Vertex 2: Top-right-back
-				{{-0.5f,  0.5f, -0.5f}},  // Vertex 3: Top-left-back
-				{{-0.5f, -0.5f,  0.5f}},  // Vertex 4: Bottom-left-front
-				{{ 0.5f, -0.5f,  0.5f}},  // Vertex 5: Bottom-right-front
-				{{ 0.5f,  0.5f,  0.5f}},  // Vertex 6: Top-right-front
-				{{-0.5f,  0.5f,  0.5f}},  // Vertex 7: Top-left-front
-			};
-			std::vector<uint32_t> indices = {
-				// Bottom face
-				0, 1,  // Edge between Vertex 0 and Vertex 1
-				1, 2,  // Edge between Vertex 1 and Vertex 2
-				2, 3,  // Edge between Vertex 2 and Vertex 3
-				3, 0,  // Edge between Vertex 3 and Vertex 0
 
-				// Top face
-				4, 5,  // Edge between Vertex 4 and Vertex 5
-				5, 6,  // Edge between Vertex 5 and Vertex 6
-				6, 7,  // Edge between Vertex 6 and Vertex 7
-				7, 4,  // Edge between Vertex 7 and Vertex 4
+		
 
-				// Vertical edges connecting bottom and top faces
-				0, 4,  // Edge between Vertex 0 and Vertex 4
-				1, 5,  // Edge between Vertex 1 and Vertex 5
-				2, 6,  // Edge between Vertex 2 and Vertex 6
-				3, 7   // Edge between Vertex 3 and Vertex 7
-			};
+		struct system: ISystem {
+			void init(Scene& scene) override {
+				std::vector<default_vertex_t> vertices = {
+								{{-0.5f, -0.5f, -0.5f}},  // Vertex 0: Bottom-left-back
+								{{ 0.5f, -0.5f, -0.5f}},  // Vertex 1: Bottom-right-back
+								{{ 0.5f,  0.5f, -0.5f}},  // Vertex 2: Top-right-back
+								{{-0.5f,  0.5f, -0.5f}},  // Vertex 3: Top-left-back
+								{{-0.5f, -0.5f,  0.5f}},  // Vertex 4: Bottom-left-front
+								{{ 0.5f, -0.5f,  0.5f}},  // Vertex 5: Bottom-right-front
+								{{ 0.5f,  0.5f,  0.5f}},  // Vertex 6: Top-right-front
+								{{-0.5f,  0.5f,  0.5f}},  // Vertex 7: Top-left-front
+				};
+				std::vector<uint32_t> indices = {
+					// Bottom face
+					0, 1,  // Edge between Vertex 0 and Vertex 1
+					1, 2,  // Edge between Vertex 1 and Vertex 2
+					2, 3,  // Edge between Vertex 2 and Vertex 3
+					3, 0,  // Edge between Vertex 3 and Vertex 0
 
-			AABB::mesh.init(vertices, indices, "AABB_shader", "resources/shaders/AABB.vert", "resources/shaders/AABB.frag");
-		}
+					// Top face
+					4, 5,  // Edge between Vertex 4 and Vertex 5
+					5, 6,  // Edge between Vertex 5 and Vertex 6
+					6, 7,  // Edge between Vertex 6 and Vertex 7
+					7, 4,  // Edge between Vertex 7 and Vertex 4
 
-		static void draw_all(entt::registry& registry, Camera& camera)
-		{
-			mesh.vao.bind();
-			AABB::mesh.shader->bind();
+					// Vertical edges connecting bottom and top faces
+					0, 4,  // Edge between Vertex 0 and Vertex 4
+					1, 5,  // Edge between Vertex 1 and Vertex 5
+					2, 6,  // Edge between Vertex 2 and Vertex 6
+					3, 7   // Edge between Vertex 3 and Vertex 7
+				};
 
-			auto view = registry.view<AABB>();
-			for (auto entity : view)
-			{
-				auto& visualizer = registry.get<AABB>(entity);
-				camera.set_shader_uniforms(AABB::mesh.shader);
-
-				AABB::mesh.shader->operator[]("u_location") = visualizer.location;
-				AABB::mesh.shader->operator[]("u_dimensions") = visualizer.dimensions;
-
-				glDrawElements(GL_LINES, 24, GL_UNSIGNED_INT, 0);
+				AABB::mesh.init(vertices, indices, "AABB_shader", "resources/shaders/AABB.vert", "resources/shaders/AABB.frag");
 			}
+
+			void tick(Scene& scene) override {
+				if (!Camera::target_camera) return;
+
+				mesh.vao.bind();
+				AABB::mesh.shader->bind();
+
+				auto view = scene.view<AABB>();
+				for (auto entity : view)
+				{
+					auto& visualizer = scene.get_component<AABB>(entity);
+					Camera::target_camera->set_shader_uniforms(AABB::mesh.shader);
+
+					AABB::mesh.shader->operator[]("u_location") = visualizer.location;
+					AABB::mesh.shader->operator[]("u_dimensions") = visualizer.dimensions;
+
+					glDrawElements(GL_LINES, 24, GL_UNSIGNED_INT, 0);
+				}
+			}
+			void destroy(Scene& scene) override {
+				// free the mesh
+				AABB::mesh.~Mesh();
+			}
+		};
+
+
+
+
+		//static void init()
+		//{
+			
+		//}
+
+		static void draw_all(Scene& scene)
+		{
+			
 		}
 
 	private:
 		// This will be excluded from mesh::draw_all, since this isn't registered in any entt::registry ; )
-		static components::mesh mesh;
+		static Mesh mesh;
 	};
 	//Shader AABB::shader;
-	components::mesh AABB::mesh;
+	Mesh AABB::mesh;
 
-} // namespace components
+}
+
+using AABB = Quakaster::components::AABB;
