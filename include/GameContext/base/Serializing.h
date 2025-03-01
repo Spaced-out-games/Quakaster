3ea808dc2eaf73c8fb58d956b6f19ed78250a5db
Devin Frost <devin.frost4311@gmail.com>
2025-03-01 17:22:55 -0500

    nah, nevermind

--- include/GameContext/base/Serializing.h
+++ include/GameContext/base/Serializing.h
@@ -6,28 +6,5 @@
 using entity_type_t = uint16_t;
 
 
-// Pass this into an entity's de/serialize method by reference, and it will read/write and advance the offset appropriately
-struct serializer_context {
-	// Pointer to the file being read, or the file created on the heap
-	uint8_t* data;
+// Instead of using a serializer class, let's just use entt::snapshot
 
-	// Offset from the pointer (in bytes) the current context is at.
-	uint32_t offset;
-
-	// The total size of the buffer pointed to.
-	uint32_t size;
-
-};
-
-struct block_header {
-
-	// The type of entity contiguously stored in this block
-	entity_type_t type;
-	
-	// Used to calculate the size of the block
-	uint8_t exponent;
-
-	size_t size() {
-		return 2 << exponent;
-	}
-};
