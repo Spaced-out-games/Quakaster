70007fa1c19de6ef80a228202c36ddd6b1e51910
Devin Frost <devin.frost4311@gmail.com>
2025-03-02 15:09:15 -0500

    Integrated exit() and update_dt() right into Application (part of a plan to generalize Application)

--- include/GameContext/client/application.h
+++ include/GameContext/client/application.h
@@ -3,16 +3,57 @@
 #include <include/GameContext/UI/UIContext.h>
 #include <include/GameContext/IO/InputDelegate.h>
 #include <include/GameContext/base/eventHandler.h>
+#include <include/GameContext/client/Client.h>
+#include <include/GameContext/base/System.h>
+
+
+
 
 struct Application
 {
-	
+	// Content window
 	Window window;
+
+	// GUI context
 	UIContext ui_context;
 
+	// IP address, player ID, sockets, client-side copy of the scene. also, client-side systems
+	Client cl;
 
-	Application(EventHandler& event_handler, ConsoleInterpreter& interpreter) : ui_context( event_handler, interpreter, window.get_renderer(), window), window(interpreter)
+	// color of the background
+	glm::vec3 bg_color = { 0.0f,1.0f,1.0f };
+
+
+	// Timing variables using std::chrono
+	std::chrono::steady_clock::time_point lastFrameTime;
+	std::chrono::steady_clock::time_point currentFrameTime;
+
+	InputDelegate input_delegate;
+
+	// Error code or exit
+	int status = 1;
+
+
+	Application(EventHandler& event_handler, ConsoleInterpreter& interpreter) : input_delegate(event_handler), ui_context( event_handler, interpreter, window.get_renderer(), window, input_delegate), window(interpreter)
 	{
+
 	}
 
+	// Updates timing information for the current frame
+	inline void update_dt()
+	{
+		currentFrameTime = std::chrono::steady_clock::now(); // Get current time
+		std::chrono::duration<float> elapsed = currentFrameTime - lastFrameTime; // Time elapsed
+		deltaTime = elapsed.count(); // Convert to seconds
+		lastFrameTime = currentFrameTime; // Update the last frame time
+	}
+
+	inline void exit() {
+		status = 0;
+	}
+
+	static const float& get_deltaTime() { return deltaTime; }
+
+	private:
+		static inline float deltaTime = 0.0f;
 };
