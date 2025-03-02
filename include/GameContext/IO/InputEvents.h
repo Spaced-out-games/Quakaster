28b274719b6a8e5f92e136b7e08167f1ff48c193
Devin Frost <devin.frost4311@gmail.com>
2025-03-02 15:12:35 -0500

    Removed MouseClickEvent modifier Added PauseEvent

--- include/GameContext/IO/InputEvents.h
+++ include/GameContext/IO/InputEvents.h
@@ -23,13 +23,25 @@ struct KeyHoldEvent { SDL_Keycode code; Uint32 duration; KeyHoldEvent(SDL_Keycod
 struct KeyReleaseEvent { SDL_Keycode code; Uint16 mod; KeyReleaseEvent(SDL_Keycode code, Uint16 mod = KMOD_NONE) : code(code), mod(mod) {} };
 
 // Triggered when a mouse button is pressed
-struct MouseClickEvent { Uint8 button; Uint16 mod; MouseClickEvent(Uint8 button, Uint16 mod = KMOD_NONE) : button(button), mod(mod) {} };
+struct MouseClickEvent {
+	Uint8 button;
+	bool down;
+	Sint32 x;
+	Sint32 y;
+	MouseClickEvent(Uint8 button, Sint32 x, Sint32 y,bool down) : button(button), down(down), x(x), y(y) {}
+};
 
 // Triggered when a mouse button is released
 struct MouseReleaseEvent { Uint8 button; Uint16 mod; MouseReleaseEvent(Uint8 button, Uint16 mod = KMOD_NONE) : button(button), mod(mod) {} };
 
 // Triggered when the mouse is moved
-struct MouseMoveEvent { int x, y; int xrel, yrel; Uint16 mod; MouseMoveEvent(int x, int y, int xrel, int yrel, Uint16 mod = KMOD_NONE) : x(x), y(y), xrel(xrel), yrel(yrel), mod(mod) {} };
+struct MouseMoveEvent {
+	glm::ivec2 current_position;
+	glm::ivec2 relative_position;
+	Uint16 mod;
+	MouseMoveEvent(glm::ivec2 current, glm::ivec2 delta) : current_position(current), relative_position(delta){
+	}
+};
 
 // Triggered when the mouse wheel is scrolled
 struct MouseScrollEvent { int x, y; Uint16 mod; MouseScrollEvent(int x, int y, Uint16 mod = KMOD_NONE) : x(x), y(y), mod(mod) {} };
@@ -42,4 +54,8 @@ struct WindowEvent {
 
 	WindowEvent(Uint8 event_type, int data1 = 0, int data2 = 0)
 		: event_type(event_type), data1(data1), data2(data2) {}
+};
+
+struct PauseEvent {
+	bool new_pause;
 };
