69942fba53374d5018ff0a01e0189e2278bb4b91
Devin Frost <devin.frost4311@gmail.com>
2025-03-02 15:10:35 -0500

    Added constructor Added control_point type

--- include/GameContext/components/Scalar.h
+++ include/GameContext/components/Scalar.h
@@ -4,9 +4,16 @@
 #include <include/thirdparty/glm/vec3.hpp>
 
 struct Scalar : Component {
+	Scalar() = default;
+	Scalar(float x, float y, float z) : scale({ x, y, z }) {}
 	glm::vec3 scale = { 1.0f,1.0f,1.0f };
 };
 
 struct UniformScalar : Component {
 	float scale = 1.0f;
+};
+
+// Good for utility components, mostly.
+struct control_point : Component {
+	glm::vec3 point = { 0.0f,0.0f,0.0f };
 };
