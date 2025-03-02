c6537f84d574f22ebcba62efc561eb75bee725e3
Devin Frost <devin.frost4311@gmail.com>
2025-03-02 15:13:12 -0500

    Renamed the interpreter and scene

--- include/GameContext/server/server.h
+++ include/GameContext/server/server.h
@@ -1,15 +1,12 @@
 #pragma once
-#include "entt.hpp"
-#include "ConsoleInterpreter.h"
-#include "scene.h"
+#include <include/thirdparty/entt.hpp>
+#include <include/GameContext/server/interpreter/ConsoleInterpreter.h>
+#include <include/GameContext/base/Scene.h>
 
 
 struct Server
 {
-	//entt::resource_cache<res_mesh> meshes;
-	//entt::resource_cache<res_texture> meshes;
-	//entt::resource_cache<res_shader> meshes;
-	ConsoleInterpreter sv_console_interpreter;
-	Scene sv_scene;
+	ConsoleInterpreter interpreter;
+	Scene scene;
 
 };
