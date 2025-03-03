16477e71b323bf9205ddf329b4d6924aa27972ae
Devin Frost <devin.frost4311@gmail.com>
2025-03-02 23:43:16 -0500

    Added a clarification for ISystem

--- include/GameContext/base/System.h
+++ include/GameContext/base/System.h
@@ -3,8 +3,8 @@
 
 namespace Quakaster::base {
 	/// <summary>
-	/// Represents a single broad-phase operation on entities. Useful for modularization. They are not meant to store the state of anything...
-	/// If you need internal state, create your own custom structure, and add it as a component. You can do this via init(Scene&), calling get_universal_state_entity(), then appending your component. Of course, the tick() and destroy() functions can function likewise
+	/// Represents a single broad-phase operation on entities. Useful for modularization. They ARE allowed to store internal state...
+
 	/// </summary>
 	struct ISystem {
 		virtual void init(Scene& scene) = 0;
