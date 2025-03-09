84e87b73e3defbe49a9c8b41f5b6e1aa5acada1e
Devin Frost <devin.frost4311@gmail.com>
2025-03-08 23:55:09 -0500

    Added some auxillary methods Removed EventHandler dependency

--- include/entities/ent_player.h
+++ include/entities/ent_player.h
@@ -10,29 +10,39 @@ namespace Quakaster::entities {
 	struct ent_player: Entity {
 
 		struct tag : public Tag {};
-		/// <summary>
-		/// Creates a player entity
-		/// </summary>
-		/// <param name="scene">The scene to add the player to</param>
-		/// <param name="interpreter">The interpreter to set console commands for</param>
-		/// <param name="event_handler"></param>
-		ent_player(Scene& scene, ConsoleInterpreter& interpreter, EventHandler& event_handler): Entity(scene) {
+		// Creates a player entity
+		ent_player(Scene& scene): Entity(scene) {
 
 			// Tag telling us this is a player
 			add_component<ent_player::tag>();
 
 			// Holds the orientation of the player
 			add_component<Transform>();
-			add_component<Camera>(get_component<Transform>());
+			add_component<Camera>(get_component<Transform>(), fov);
 			add_component<AABB>(get_component<Transform>().position);
 			add_component<Scalar>(0.02,0.02,0.02);
 			// set up the camera
 			get_component<Camera>().set_target(*this);
-			get_component<Camera>().bind_convars(interpreter);
 			add_component<MoveState>();
-			add_component<test_controller>(event_handler, *this);
+			add_component<test_controller>(*this);
 			add_component<vector_visualizer>(get_component<MoveState>().velocity(), get_component<Camera>().owner_transform);
 		}
+
+		// gets the Transform
+		inline Transform& transform() { return get_component<Transform>(); }
+		
+		// Get the Camera
+		inline Camera& camera() { return get_component<Camera>(); }
+		
+		// gets the axis-aligned bounding box
+		inline AABB& aabb() { return get_component<AABB>(); }
+
+		// gets the scalar for use by the vector_visualizer
+		inline Scalar& scalar() { return get_component<Scalar>(); }
+
+		// gets the move state for use by the physics simulation and input
+
+
 	};
 }
 
