a8211186ff27cdfbac17ed74238217f18a14faab
Devin Frost <devin.frost4311@gmail.com>
2025-03-08 23:45:30 -0500

    Updated the event system Added documentation

--- include/client/application.h
+++ include/client/application.h
@@ -2,7 +2,7 @@
 #include <include/window/window.h>
 #include <include/UI/UIContext.h>
 #include <include/IO/InputDelegate.h>
-#include <include/base/eventHandler.h>
+#include <include/base/Event.h>
 #include <include/client/Client.h>
 #include <include/base/System.h>
 
@@ -25,59 +25,54 @@ public:
 	static const float& get_deltaTime() { return deltaTime; }
 	static const float& get_elapsedTime() { return elapsedTime; }
 
-protected:
-	// Content window
-	Window window;
-
-	// IP address, player ID, sockets, client-side copy of the scene. also, client-side systems
-	Client cl;
-
-	// color of the background
-	glm::vec3 bg_color = { 0.0f,1.0f,1.0f };
 
 
-	// Timing variables using std::chrono
-	std::chrono::steady_clock::time_point lastFrameTime;
-	std::chrono::steady_clock::time_point currentFrameTime;
+	
+	protected:
+		Client cl;
+
+		// IP address, player ID, sockets, client-side copy of the scene. also, client-side systems
+		
+
+		// color of the background
+		glm::vec3 bg_color = { 0.0f,1.0f,1.0f };
 
 
-	// Error code or exit
-	int status = 1;
-
-	// Systems to run the simulation
-	//std::vector<ISystem*> systems;
-
-	std::vector<Entity*> entities;
+		// Timing variables
+		std::chrono::steady_clock::time_point lastFrameTime;
+		std::chrono::steady_clock::time_point currentFrameTime;
 
 
-	static inline EventHandler event_handler;
+		// Error code or exit
+		int status = 1;
+
+		// list of entities, might want to remove. Mostly for keeping things in 
+		std::vector<Entity*> entities;
 
 
-
-
-
-	inline void refresh() const { SDL_GL_SwapWindow(window.sdl_window); }
+		// Content window
+		Window window;
 
 
 
-	Application(ConsoleInterpreter& interpreter) : window(interpreter)
-	{
+		// Refreshes the screen
+		inline void refresh() const { SDL_GL_SwapWindow(window.sdl_window); }
 
-	}
+		Application() {}
 
-	// Updates timing information for the current frame
-	inline void update_dt()
-	{
-		currentFrameTime = std::chrono::steady_clock::now(); // Get current time
-		std::chrono::duration<float> elapsed = currentFrameTime - lastFrameTime; // Time elapsed
-		deltaTime = elapsed.count(); // Convert to seconds
-		elapsedTime += deltaTime;
-		lastFrameTime = currentFrameTime; // Update the last frame time
-	}
+		// Updates timing information for the current frame
+		inline void update_dt()
+		{
+			currentFrameTime = std::chrono::steady_clock::now(); // Get current time
+			std::chrono::duration<float> elapsed = currentFrameTime - lastFrameTime; // Time elapsed
+			deltaTime = elapsed.count(); // Convert to seconds
+			elapsedTime += deltaTime;
+			lastFrameTime = currentFrameTime; // Update the last frame time
+		}
 
-	inline void exit() {
-		status = 0;
-	}
+		inline void exit() {
+			status = 0;
+		}
 
 
 	private:
