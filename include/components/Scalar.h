e7341eec7e6dbe7c800336f578a8377bc18af5ff
Devin Frost <devin.frost4311@gmail.com>
2025-03-08 23:49:09 -0500

    Made Scalar inherit from glm::vec3

--- include/components/Scalar.h
+++ include/components/Scalar.h
@@ -3,10 +3,11 @@
 #include <include/base/System.h>
 #include <include/thirdparty/glm/vec3.hpp>
 
-struct Scalar : Component {
+struct Scalar : Component, glm::vec3 {
 	Scalar() = default;
-	Scalar(float x, float y, float z) : scale({ x, y, z }) {}
-	glm::vec3 scale = { 1.0f,1.0f,1.0f };
+	Scalar(float x, float y, float z) : glm::vec3(x,y,z) {}
+private:
+	//glm::vec3 scale = { 1.0f,1.0f,1.0f };
 };
 
 // Good for utility components like vector_visualizer and AABB's rendering
