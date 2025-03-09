d5602cfedfddde13c8b73e11eaa08269a9852777
Devin Frost <devin.frost4311@gmail.com>
2025-03-09 00:01:26 -0500

    Made the members private Updated EventListener to use the new event system Added a function to determine if ImGui is initialized Bunch more functions

--- include/IO/InputDelegate.h
+++ include/IO/InputDelegate.h
@@ -1,14 +1,13 @@
 #pragma once
 #include "InputEvents.h"
-#include <include/base/eventHandler.h>
+#include <include/base/Event.h>
 #include <include/base/System.h>
 #include <include/server/interpreter/ConsoleInterpreter.h>
 #include <include/UI/UIContext.h>
 #include <include/utils/utility_types.h>
 #include <include/IO/InputEvents.h>
 #include <unordered_map>
-#include <imgui.h>
-
+#include <include/common.h>
 
 struct IOState {
 	glm::ivec2 last_mouse;
@@ -20,9 +19,9 @@ template <typename event_t>
 struct EventListener {
 	std::function<void(event_t)> on_event;
 
-	EventListener(EventHandler& dispatcher, std::function<void(event_t)> on_event = [](event_t) {}) : on_event(on_event)
+	EventListener(std::function<void(event_t)> on_event = [](event_t) {}) : on_event(on_event)
 	{
-		dispatcher.sink<event_t>().connect<&EventListener::onEvent>(this);
+		Event::dispatcher.sink<event_t>().connect<&EventListener::onEvent>(this);
 	}
 
 private:
@@ -34,24 +33,29 @@ private:
 
 // Fires input events that controllers and the like can hook onto
 struct InputDelegate : ISystem {
-	InputDelegate(EventHandler& handler) : event_handler(handler) {}
-	// For storing immediate SDL Events as they poll.
-	SDL_Event event; // was event
-
-	// For handling ImGUI state information
-	ImGuiIO* ImGuiState = nullptr; // was ImGuiState
-
-	// This is how we dispatch events
-	EventHandler& event_handler; // was dispatcher
-
-	IOState KeyboardMouseState; // was lastMouseX, lastMouseY, keyStates
-	bool is_paused = false;
-	bool last_pause_state = false;
 
 	bool isImGuiInitialized() const { return ImGui::GetCurrentContext() != nullptr; }
 	void init(Scene& scene) override;
 	void tick(Scene& scene) override;
 	void destroy(Scene& scene) override {};
+	inline void togglePause() { m_IsPaused ^= 1; }
+	inline bool& is_paused() { return m_IsPaused; }
+	inline bool& was_paused() { return last_pause_state; }
+
+	private:
+
+		// For storing immediate SDL Events as they poll.
+		SDL_Event event; // was event
+
+		// For handling ImGUI state information
+		ImGuiIO* ImGuiState = nullptr; // was ImGuiState
+
+		// This is how we dispatch events
+
+		IOState KeyboardMouseState; // was lastMouseX, lastMouseY, keyStates
+		bool m_IsPaused = false;
+		bool last_pause_state = true;
+
 };
 
 
@@ -59,6 +63,7 @@ struct InputDelegate : ISystem {
 
 void InputDelegate::init(Scene& scene) {
 	ImGuiState = &ImGui::GetIO();
+	SDL_SetRelativeMouseMode(is_paused() ? SDL_TRUE : SDL_FALSE); // make sure it's synced
 }
 
 void InputDelegate::tick(Scene& scene) {
@@ -72,52 +77,52 @@ void InputDelegate::tick(Scene& scene) {
 	// start polling
 	while (SDL_PollEvent(&event))
 	{
-		// ESC key always intercepts events, because regardless of pause state, it should invert the pause state
+		// ESC key always intercepts events, because regardless of current pause state, it should invert the pause state. Might be better to make this not hard-coded so we can actually bind custom shit
 		if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE)
 		{
-			is_paused = !is_paused;
-			if (!is_paused) last_pause_state = 0;
-			SDL_SetRelativeMouseMode(is_paused ? SDL_TRUE : SDL_FALSE);
+			togglePause();
+			//if (!is_paused()) last_pause_state = 0;
+			SDL_SetRelativeMouseMode(is_paused() ? SDL_TRUE : SDL_FALSE);
 			// Fire a pause event
-			//event_handler.trigger(PauseEvent{});
+			// Event::fire(PauseEvent{});
 
 			// we are done, so return
 			return;
 		}
 
-		std::cout << is_paused << '\n';
+		LOG(is_paused() << '\n');
 
 		// Forward it to ImGui
 		ImGui_ImplSDL2_ProcessEvent(&event);
 
 		// Handle whatever ImGui rejects
-		if ((!ImGuiState->WantCaptureKeyboard || !ImGuiState->WantCaptureMouse) && is_paused)
+		if ((!ImGuiState->WantCaptureKeyboard || !ImGuiState->WantCaptureMouse) && is_paused())
 		{
 			switch (event.type)
 			{
 			case SDL_KEYDOWN:
-				event_handler.trigger(KeyPressEvent{ event.key.keysym.sym, event.key.keysym.mod });
+				Event::fire<KeyPressEvent>(event.key.keysym.sym, event.key.keysym.mod);
 				KeyboardMouseState.key_states[static_cast<int>(event.key.keysym.sym)] = 1;
 				break;
 			case SDL_KEYUP:
-				event_handler.trigger(KeyReleaseEvent{ event.key.keysym.sym, event.key.keysym.mod });
+				Event::fire<KeyReleaseEvent>(event.key.keysym.sym, event.key.keysym.mod);
 				// Removes the key from the hash map so it's not iterated over anymore
 				KeyboardMouseState.key_states.erase(static_cast<int>(event.key.keysym.sym)); 
 				break;
 			case SDL_MOUSEBUTTONDOWN:
-				event_handler.trigger(MouseClickEvent{ event.button.button, event.button.x, event.button.y, true });
+				Event::fire<MouseClickEvent>(event.button.button, event.button.x, event.button.y, true);
 				break;
 			case SDL_MOUSEBUTTONUP:
-				event_handler.trigger(MouseClickEvent{ event.button.button, event.button.x, event.button.y, false });
+				Event::fire<MouseClickEvent>(event.button.button, event.button.x, event.button.y, false);
 				break;
 			case SDL_WINDOWEVENT:
-				event_handler.trigger(WindowEvent{ event.window.event });
+				Event::fire<WindowEvent>(event.window.event);
 				break;
 			case SDL_MOUSEMOTION:
 				// Get mouse motion details
 				KeyboardMouseState.last_mouse = { event.motion.x, event.motion.y };
 				KeyboardMouseState.rel_mouse = { event.motion.xrel, event.motion.yrel };
-				event_handler.trigger(MouseMoveEvent{ KeyboardMouseState.last_mouse, KeyboardMouseState.rel_mouse });
+				Event::fire<MouseMoveEvent>(KeyboardMouseState.last_mouse, KeyboardMouseState.rel_mouse);
 
 				break;
 
@@ -137,7 +142,7 @@ void InputDelegate::tick(Scene& scene) {
 				if (isPressed)
 				{
 					// Trigger a repeat event for the pressed key
-					event_handler.trigger(KeyHoldEvent{ key });
+					Event::fire<KeyHoldEvent>(key);
 				}
 			}
 		}
