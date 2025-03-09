56adc85a836264fa544f7de8b8e164466a6c329d
Devin Frost <devin.frost4311@gmail.com>
2025-03-08 23:41:20 -0500

    Added SystemManager.h

---
+++ include/base/SystemManager.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <vector>
+#include <include/base/System.h>
+#include <functional>
+
+
+struct SystemManager {
+public:
+    // Access systems using the operator[]
+    ISystem* operator[] (size_t index) {
+        return systems.at(index);
+    }
+
+
+
+    //inline void init_all_systems()
+
+    const std::vector<ISystem*>& get_systems() {
+        return systems;
+    }
+
+    // Add a new system
+    template <typename system_t, typename... Args>
+    void add_system(Args&&... args) {
+        systems.push_back(new system_t(std::forward<Args>(args)...)); // Allocate memory for the new system
+        //systems.back()->init(); // Call init on the newly added system
+    }
+
+    // Destructor to clean up allocated systems
+    ~SystemManager() {
+        for (auto system : systems) {
+            delete system; // Free the allocated memory
+        }
+    }
+protected:
+    friend class Client;
+    friend class Server;
+    inline void init_system(int index, Scene& scene) const {
+        systems[index]->init(scene);
+    }
+
+    inline void tick_system(int index, Scene& scene) const {
+        systems[index]->tick(scene);
+    }
+
+    inline void destroy_system(int index, Scene& scene) const {
+
+    }
+private:
+    std::vector<ISystem*> systems; // Store raw pointers to systems
+};
