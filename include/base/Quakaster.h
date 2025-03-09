3a8e14073b9ddd138831fa11aa720cfa9fa6ba21
Devin Frost <devin.frost4311@gmail.com>
2025-03-08 23:37:57 -0500

    Added base.h Removed Quakaster.h

--- include/base/Quakaster.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <include/base/Component.h>
-#include <include/base/Entity.h>
-#include <include/base/Scene.h>
-#include <include/base/Tag.h>
-#include <include/base/Serializing.h>
-#include <include/base/System.h>
