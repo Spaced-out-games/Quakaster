edc3b743eca55b23bacdc3ce8b79f7d137973ed6
Devin Frost <devin.frost4311@gmail.com>
2025-03-09 00:04:45 -0500

    Major console redesign

--- include/server/interpreter/console_function.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <include/utils/utility_types.h>
-
-struct console_message;
-class Token;
-class ConsoleInterpreter;
-
-using console_fn = func_ptr_t<void, console_message&, ConsoleInterpreter&, std::span<Token>>;
