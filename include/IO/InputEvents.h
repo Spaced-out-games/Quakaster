d8214bfdb838f979549ecf0efa2b2adfd2d15d3d
Devin Frost <devin.frost4311@gmail.com>
2025-03-09 00:02:41 -0500

    Removed SDL_Utils MAde all the events actually derive from Event
    Added a TickEvent (not actually implemented yet)

--- include/IO/InputEvents.h
+++ include/IO/InputEvents.h
@@ -2,28 +2,20 @@
 #include <sstream>
 #include <SDL.h>
 #include <include/UI/console_message.h>
+#include <include/base/Event.h>
 
-struct SDL_Utils {
-public:
-	// Converts a key code to its representation
-	static const char* repr(SDL_Keycode keycode) {
-		static const char* unknown = "[UNKNOWN]";
-		const char* keyName = SDL_GetKeyName(keycode);
-		return keyName ? keyName : unknown;
-	}
-};
 
 // Triggered when a key is pressed
-struct KeyPressEvent { SDL_Keycode code; Uint16 mod; KeyPressEvent(SDL_Keycode code, Uint16 mod = KMOD_NONE) : code(code), mod(mod) {} };
+struct KeyPressEvent: Event { SDL_Keycode code; Uint16 mod; KeyPressEvent(SDL_Keycode code, Uint16 mod = KMOD_NONE) : code(code), mod(mod) {} };
 
 // Triggered when a key is held down
-struct KeyHoldEvent { SDL_Keycode code; Uint32 duration; KeyHoldEvent(SDL_Keycode code) : code(code), duration(duration) {} };
+struct KeyHoldEvent: Event { SDL_Keycode code; Uint32 duration; KeyHoldEvent(SDL_Keycode code) : code(code), duration(duration) {} };
 
 // Triggered when a key is released
-struct KeyReleaseEvent { SDL_Keycode code; Uint16 mod; KeyReleaseEvent(SDL_Keycode code, Uint16 mod = KMOD_NONE) : code(code), mod(mod) {} };
+struct KeyReleaseEvent: Event { SDL_Keycode code; Uint16 mod; KeyReleaseEvent(SDL_Keycode code, Uint16 mod = KMOD_NONE) : code(code), mod(mod) {} };
 
 // Triggered when a mouse button is pressed
-struct MouseClickEvent {
+struct MouseClickEvent: Event {
 	Uint8 button;
 	bool down;
 	Sint32 x;
@@ -32,10 +24,10 @@ struct MouseClickEvent {
 };
 
 // Triggered when a mouse button is released
-struct MouseReleaseEvent { Uint8 button; Uint16 mod; MouseReleaseEvent(Uint8 button, Uint16 mod = KMOD_NONE) : button(button), mod(mod) {} };
+struct MouseReleaseEvent: Event { Uint8 button; Uint16 mod; MouseReleaseEvent(Uint8 button, Uint16 mod = KMOD_NONE) : button(button), mod(mod) {} };
 
 // Triggered when the mouse is moved
-struct MouseMoveEvent {
+struct MouseMoveEvent : Event {
 	glm::ivec2 current_position;
 	glm::ivec2 relative_position;
 	Uint16 mod;
@@ -44,10 +36,10 @@ struct MouseMoveEvent {
 };
 
 // Triggered when the mouse wheel is scrolled
-struct MouseScrollEvent { int x, y; Uint16 mod; MouseScrollEvent(int x, int y, Uint16 mod = KMOD_NONE) : x(x), y(y), mod(mod) {} };
+struct MouseScrollEvent : Event { int x, y; Uint16 mod; MouseScrollEvent(int x, int y, Uint16 mod = KMOD_NONE) : x(x), y(y), mod(mod) {} };
 
 // Triggered when the window state changes
-struct WindowEvent {
+struct WindowEvent : Event {
 	Uint8 event_type;   // Type of window event (e.g., SDL_WINDOWEVENT_RESIZED, SDL_WINDOWEVENT_FOCUS_GAINED, etc.)
 	int data1;          // Data associated with the event (e.g., width for resize)
 	int data2;          // Additional data (e.g., height for resize)
@@ -56,6 +48,10 @@ struct WindowEvent {
 		: event_type(event_type), data1(data1), data2(data2) {}
 };
 
-struct PauseEvent {
+struct PauseEvent : Event {
 	bool new_pause;
+};
+
+struct TickEvent : Event {
+	float tick_interval;
 };
