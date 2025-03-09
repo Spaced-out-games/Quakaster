39e28ed99a6afc8857bedccde28fd84b07ec5e0b
Devin Frost <devin.frost4311@gmail.com>
2025-03-08 23:48:25 -0500

    Removed EventHandler dependency

--- include/components/Controller.h
+++ include/components/Controller.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <include/components/transform.h>
 #include <include/IO/InputDelegate.h>
-#include <include/base/eventHandler.h>
+#include <include/base/Event.h>
 #include <include/components/camera.h>
 #include <include/thirdparty/entt.hpp>
 #include <math.h>
@@ -9,17 +9,16 @@
 
 struct Controller {
 
-	Controller(EventHandler& event_handler,
+	Controller(
 		std::function<void(KeyPressEvent)> on_key_press_fn = [](KeyPressEvent) {},
 		std::function<void(KeyHoldEvent)> on_key_hold_fn = [](KeyHoldEvent) {},
 		std::function<void(KeyReleaseEvent)> on_key_release_fn = [](KeyReleaseEvent) {},
 		std::function<void(MouseMoveEvent)> on_mouse_move_fn = [](MouseMoveEvent) {}
 	) :
-		event_handler(event_handler),
-		on_key_press(event_handler, on_key_press_fn),
-		on_key_hold(event_handler, on_key_hold_fn),
-		on_key_release(event_handler, on_key_release_fn),
-		on_mouse_move(event_handler, on_mouse_move_fn)
+		on_key_press(on_key_press_fn),
+		on_key_hold(on_key_hold_fn),
+		on_key_release(on_key_release_fn),
+		on_mouse_move(on_mouse_move_fn)
 	{}
 
 	protected:
@@ -27,7 +26,6 @@ struct Controller {
 		EventListener<KeyHoldEvent> on_key_hold;
 		EventListener<KeyReleaseEvent> on_key_release;
 		EventListener<MouseMoveEvent> on_mouse_move;
-		EventHandler& event_handler;
 
 	//private:
 
