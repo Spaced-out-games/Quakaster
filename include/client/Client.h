aad55a3139cd21c68ee5bfcf902500472ad1d6a8
Devin Frost <devin.frost4311@gmail.com>
2025-03-08 23:46:08 -0500

    Made the player ID protected

--- include/client/Client.h
+++ include/client/Client.h
@@ -2,19 +2,49 @@
 #include <include/thirdparty/entt.hpp>
 #include <include/base/Scene.h>
 #include <include/UI/UIContext.h>
+#include <include/base/SystemManager.h>
+#include <include/server/interpreter/Convar.h>
 
 // Minimal client
-struct Client
+struct Client : SystemManager
 {
-	//Client(EventHandler& cl_evt_handler) : delegate(cl_evt_handler) {}
-	// client-side scene
+	Client() {
+		add_convar("fov_desired", 70.0f);
+
+	}
+
+	friend class Application;
+
+	inline void init_system(int index) {
+		SystemManager::init_system(index, scene);
+	}
+
+	inline void init_all() {
+		for (ISystem* system : systems) {
+			system->init(scene);
+		}
+	}
+	inline void tick_all() {
+		for (ISystem* system : systems) {
+			system->tick(scene);
+		}
+	}
+
+
+	inline void tick_system(int index) {
+		SystemManager::tick_system(index, scene);
+	}
+
+	inline void destroy_system(int index) {
+		SystemManager::destroy_system(index, scene);
+	}
 	Scene scene;
 
-	// Temporary player ID.
-	entt::entity player_ID;
 
-	// Client-side systems
-	std::vector<ISystem*> systems;
+	protected:
+		// client-side scene
 
-	//InputDelegate delegate;
+		// Temporary player ID.
+		entt::entity player_ID;
+
 };
