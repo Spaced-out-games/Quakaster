792dcd80a79ab41ec9b6712e0bccb2b85a4cbbae
Devin Frost <devin.frost4311@gmail.com>
2025-03-09 00:14:51 -0500

    Now uses base.h instead of the old Quakaster.h

--- include/common.h
+++ include/common.h
@@ -1,6 +1,6 @@
 #pragma once
 // Include all the base 
-#include <include/base/Quakaster.h>
+#include <include/base/base.h>
 
 /// Basically an IDE hack. Allows you to basically wrap and name scopes within VS Code, my editor of choice.
 /// It is nothing more than a no-op but tricks Intellisense into thinking it's a funciton when it's not
