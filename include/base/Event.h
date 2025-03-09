6f38e5c0aaa637d9407c95aad237fb04ff4aed72
Devin Frost <devin.frost4311@gmail.com>
2025-03-08 23:39:38 -0500

    Properly wrapped the entt::dispatcher

---
+++ include/base/Event.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <iostream>
+#include <chrono>
+#include <include/thirdparty/entt.hpp>
+
+struct EventHandler {};
+
+struct Event {
+    Event()
+        : timestamp(std::chrono::duration_cast<std::chrono::microseconds>(
+            std::chrono::system_clock::now().time_since_epoch())) {
+        std::cout << timestamp.count() << " microseconds since epoch\n";
+    }
+
+    template <typename T, typename... Args>
+    static void fire(Args&&... args) {
+        dispatcher.trigger<T>(T(std::forward<Args>(args)...)); // Forward arguments to the dispatcher
+    }
+
+    template <typename event_t, typename invoker_t, auto func>
+    static void subscribe(invoker_t* this_ptr) {
+        dispatcher.sink<event_t>().connect<func>(this_ptr);
+    }
+
+        static inline entt::dispatcher dispatcher;
+    private:
+
+        std::chrono::microseconds timestamp;
+};
+
