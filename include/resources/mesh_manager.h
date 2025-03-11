334fc1791807ee2f7323d0228a5b697300e86dac
Devin Frost <devin.frost4311@gmail.com>
2025-03-11 00:56:40 -0400

    Added get_matrix(), submit(), and added forward methods for this to MeshInstance

--- include/resources/mesh_manager.h
+++ include/resources/mesh_manager.h
@@ -9,9 +9,6 @@
 #include <GL/glew.h>
 #include <include/resources/res_shader.h>
 #include <include/thirdparty/tiny_obj_loader.h>
-/// <summary>
-/// Defines per-mesh state information
-/// </summary>
 struct MeshInfo {
 	MeshInfo() = default;
 
@@ -53,30 +50,9 @@ struct MeshInfo {
 
 };
 
-struct MeshInstance {
-
-	void bind() const { glBindVertexArray(vao); }
-	static void unbind() { glBindVertexArray(0); }
 
 
-	// delete copy constructors...might change this in the future for convenience
-	//MeshInstance(const MeshInstance&) = delete;
-	//MeshInstance& operator=(const MeshInstance&) = delete;
-
-	// delete move constructors
-	//MeshInstance(MeshInstance&&) = delete;
-	//MeshInstance& operator(MeshInstance&&) = delete;
-
-//private:
-	// cannot be constructed except by MeshManager
-	friend class MeshManager;
-	MeshInstance() = default;
-	MeshInstance(GLuint vao, uint32_t transform_index) : vao(vao), transform_index(transform_index) {}
-
-	GLuint vao = 0; // no harm no foul copy of it
-	uint32_t transform_index = 0; // okay maybe some foul
-
-};
+struct MeshInstance;
 
 
 struct MeshManager {
@@ -84,92 +60,145 @@ struct MeshManager {
 	static inline std::unordered_map<std::string, GLuint> name_map;
 	static inline std::unordered_map<GLuint, MeshInfo> info_map;
 
+	static glm::mat4 get_matrix(GLuint vao, size_t instance_index);
+	static void submit(GLuint vao, size_t instance_index, const glm::mat4& matrix);
 
-	static void submit(GLuint vao, size_t instance_index, const glm::mat4& matrix) {
-
-		glBindVertexArray(vao);
-		// get the right vbo
-
-
-		info_map[vao].instance_matrix_buffer.bind();
-		glBufferSubData(GL_ARRAY_BUFFER, instance_index * sizeof(glm::mat4), sizeof(glm::mat4), &matrix);
-	}
-
-	template <typename vertex_t = default_vertex_t>
-	static MeshInstance generate_mesh(
+	template <typename vertex_t = default_vertex_t, typename instance_t = glm::mat4>
+	static inline MeshInstance generate_mesh(
 		const std::string& name,
 		const std::vector<vertex_t>& vertices,
 		const std::vector<uint32_t>& indices,
 		std::string shader_name,
 		std::string vertex_path,
 		std::string fragment_path,
-		uint32_t num_instances = 5,
+		uint32_t num_instances = 2,
 		GLenum primitive_type = GL_TRIANGLES
-	) {
-		// check for the existence of this mesh already existing, but we will skip it for now
-		if (name_map.find(name) != name_map.end()) {
-			GLuint& vao = name_map[name];
-			MeshInfo& info = info_map[vao];
-			return MeshInstance(vao, info.next_instance_index++);
+	);
 
+};
+
+
+#include <include/components/MeshInstance.h>
+
+void MeshManager::submit(GLuint vao, size_t instance_index, const glm::mat4& matrix) {
+	glBindVertexArray(vao);
+	// get the right vbo
+
+
+	info_map[vao].instance_matrix_buffer.bind();
+	glBufferSubData(GL_ARRAY_BUFFER, instance_index * sizeof(glm::mat4), sizeof(glm::mat4), &matrix);
+}
+
+
+template <typename vertex_t = default_vertex_t, typename instance_t = glm::mat4>
+static inline MeshInstance MeshManager::generate_mesh(
+	const std::string& name,
+	const std::vector<vertex_t>& vertices,
+	const std::vector<uint32_t>& indices,
+	std::string shader_name,
+	std::string vertex_path,
+	std::string fragment_path,
+	uint32_t num_instances,
+	GLenum primitive_type
+) {
+	// check for the existence of this mesh already existing, but we will skip it for now
+	if (name_map.find(name) != name_map.end()) {
+		GLuint& vao = name_map[name];
+		MeshInfo& info = info_map[vao];
+		MeshInstance instance(vao, info.next_instance_index++);
+		//info.last_instance = &instance;
+		return instance;
+
+	}
+	std::vector<instance_t> modelMatrices(num_instances);
+
+
+
+	if constexpr (!std::is_same_v<instance_t, glm::mat4>) {
+		for (auto& instance : modelMatrices) {
+			instance = instance_t();
 		}
-		std::vector<glm::mat4> modelMatrices(num_instances, glm::mat4(1.0f));
-		GLuint vertex_pointer_index;
-		// init the vao
-		GLuint vao;
+	}
+	else {
+		for (auto& instance : modelMatrices) {
+			instance = glm::mat4(1.0f);
+		}
+	}
 
-		glGenVertexArrays(1, &vao);
-		glBindVertexArray(vao);
+	GLuint vertex_pointer_index;
+	// init the vao
+	GLuint vao;
 
-		// set up human-readible associations
-		name_map[name] = vao;
-		info_map[vao].index_count = indices.size();
-		info_map[vao].vertex_count = vertices.size();
-		info_map[vao].primitive_type = primitive_type;
-		info_map[vao].capacity = num_instances;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
 
-		// might want to remove these
-		info_map[vao].shader.init(shader_name, vertex_path, fragment_path);
-		info_map[vao].texture = new Texture(std::string("resources/images/atlas.png"));
-		
-		// initialize instance buffer
-		info_map[vao].instance_matrix_buffer.init(modelMatrices);
-		info_map[vao].instance_matrix_buffer.bind();
+	// set up human-readible associations
+	name_map[name] = vao;
+	info_map[vao].index_count = indices.size();
+	info_map[vao].vertex_count = vertices.size();
+	info_map[vao].primitive_type = primitive_type;
+	info_map[vao].capacity = num_instances;
 
-		// and its pointers
+	// might want to remove these
+	info_map[vao].shader.init(shader_name, vertex_path, fragment_path);
+	info_map[vao].texture = new Texture(std::string("resources/images/atlas.png"));
+
+	// initialize instance buffer
+	info_map[vao].instance_matrix_buffer.init(modelMatrices);
+	info_map[vao].instance_matrix_buffer.bind();
+
+	// and its pointers
+	if constexpr (std::is_same_v<instance_t, glm::mat4>) {
 		for (vertex_pointer_index = 0; vertex_pointer_index < 4; vertex_pointer_index++) {
 			glVertexAttribPointer(vertex_pointer_index, 4, GL_FLOAT, GL_FALSE, 16 * sizeof(float), (void*)(vertex_pointer_index * 4 * sizeof(float)));
 			glEnableVertexAttribArray(vertex_pointer_index);
 			glVertexAttribDivisor(vertex_pointer_index, 1); // Update per instance
 			std::cout << vertex_pointer_index;
 		}
-
-		
-
-
-		// bind vbo
-		info_map[vao].vbo.init(vertices);
-		info_map[vao].vbo.bind();
-
-		// bind ebo
-		info_map[vao].ebo.init(indices);
-		info_map[vao].ebo.bind();
-
-
-		vertex_t::set_pointers(vertex_pointer_index); // set the pointers for the custom vertex type
-
-
-
-
-		// unbind the VAO only
-		VAO::unbind();
-
-		return MeshInstance(vao, info_map[vao].next_instance_index++);
-
-
 	}
 
-};
+
+
+	// bind vbo
+	info_map[vao].vbo.init(vertices);
+	info_map[vao].vbo.bind();
+
+	// bind ebo
+	info_map[vao].ebo.init(indices);
+	info_map[vao].ebo.bind();
+
+
+	vertex_t::set_pointers(vertex_pointer_index); // set the pointers for the custom vertex type
+
+
+
+
+	// unbind the VAO only
+	VAO::unbind();
+	MeshInstance instance(vao, info_map[vao].next_instance_index++);
+	//info_map[vao].last_instance = &instance;
+	return instance;
+
+
+}
+
+
+glm::mat4 MeshManager::get_matrix(GLuint vao, size_t instance_index) {
+	if (info_map.find(vao) == info_map.end()) {
+		throw std::runtime_error("VAO not found in MeshManager!");
+	}
+
+	MeshInfo& info = info_map[vao];
+	glm::mat4 matrix;
+
+	info.instance_matrix_buffer.bind();
+	glGetBufferSubData(GL_ARRAY_BUFFER, instance_index * sizeof(glm::mat4), sizeof(glm::mat4), &matrix);
+
+	return matrix;
+}
+
+
+// totally unrelated -------------------------------------------------------------------------------------
 
 void draw_all_meshes() {
 	for (const auto& pair : MeshManager::info_map) {
