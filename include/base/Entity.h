9349607f8afa627a19886c6eaf21a89168c83c89
Devin Frost <devin.frost4311@gmail.com>
2025-03-14 12:22:05 -0400

    New naming convention. I love it Renamed get_component() to get() for brevity Likewise for add() Updated destructor to not destroy the entity if the handle goes out of scope

--- include/base/Entity.h
+++ include/base/Entity.h
@@ -1,4 +1,4 @@
-// Entity.h
+// QKEntity.h
 // PURPOSE: Define a basic wrapper around entt::
 
 #pragma once
@@ -8,89 +8,101 @@
 #include <include/base/Scene.h>
 #include <include/base/logging.h>
 
+using QKActorChildren = std::vector<entt::entity>;
 
 
 
-namespace Quakaster::base {
+
+
+/// <summary>
+/// Base entity type written over EnTT. It represents a collection of components guaranteed to exist, 
+/// as well as a series of methods that wrap inter-component functionality.
+/// </summary>
+struct QKEntity {
+// protected: //Temporarily commented while we make sure the constructors work
+
 
     /// <summary>
-    /// Base entity type written over EnTT. It represents a collection of components guaranteed to exist, 
-    /// as well as a series of methods that wrap inter-component functionality.
+    /// Initializes a new instance of the QKEntity class.
     /// </summary>
-    struct Entity {
-    // protected: //Temporarily commented while we make sure the constructors work
+    /// <param name="scene">Reference to the scene that this entity belongs to.</param>
+    QKEntity(QKScene& scene) : scene(scene), ID(scene.create()) {}
+
+    QKEntity(QKScene& scene, entt::entity existing_entity) : scene(scene), ID(existing_entity) {}
+
+    // Destructor for QKEntity. Cleans up resources.
+    virtual ~QKEntity() {};
+
+    void destroy() {
+        scene.destroy(ID);
+    }
+
+    // Gets the unique identifier for this entity.
+    entt::entity get_ID() { return ID; }
 
 
-        /// <summary>
-        /// Initializes a new instance of the Entity class.
-        /// </summary>
-        /// <param name="scene">Reference to the scene that this entity belongs to.</param>
-        Entity(Scene& scene) : scene(scene), ID(scene.registry.create()) {}
-
-        // Destructor for Entity. Cleans up resources.
-        virtual ~Entity() {
-            scene.registry.destroy(ID);
-        };
-
-        // Gets the unique identifier for this entity.
-        entt::entity get_ID() { return ID; }
-
-
-        // Gets a reference to the scene this entity belongs to.
-        Scene& get_scene() { return scene; }
+    // Gets a reference to the scene this entity belongs to.
+    QKScene& get_scene() { return scene; }
 
 
 
-        // Gets a component from this entity, if it exists, otherwise, failure
-        template <typename T>
-        inline T& get_component()
-        { return scene.get_component<T>(ID); }
+    // Gets a component from this entity, if it exists, otherwise, failure
+    template <typename T>
+    inline T& get()
+    { return scene.get<T>(ID); }
 
 
-        // Gets a component from this entity. Returns nullptr on failure
-        template <typename T>
-        inline T* try_get_component()
-        { return scene.try_get_component<T>(ID); }
+    // Gets a component from this entity. Returns nullptr on failure
+    template <typename T>
+    inline T* try_get()
+    { return scene.try_get<T>(ID); }
 
-        // Removes the component
-        template <typename T>
-        inline void remove_component() { scene.remove_component<T>(); }
+    // Removes the component
+    template <typename T>
+    inline void remove_component() { scene.remove_component<T>(); }
 
 
-        // Adds a component to this entity
-        template <typename T, typename ...Args>
-        inline void add_component(Args&&... args) { scene.add_component<T>(ID, std::forward<Args>(args)...); }
+    // Adds a component to this entity
+    template <typename T, typename ...Args>
+    inline void add(Args&&... args) { scene.add<T>(ID, std::forward<Args>(args)...); }
 
         
 
-        // Returns true if the entity has all of the requested components
-        template <typename ...Ts>
-        inline bool has() { return scene.has_all_of<Ts...>(ID); }
+    // Returns true if the entity has all of the requested components
+    template <typename ...Ts>
+    inline bool has() { return scene.has_all_of<Ts...>(ID); }
 
 
 
-        // Returns true if the entity has at least one of the following components
-        template <typename ...Ts>
-        inline bool has_one_of() { return scene.has_one_of<Ts...>(ID); }
+    // Returns true if the entity has at least one of the following components
+    template <typename ...Ts>
+    inline bool has_one_of() { return scene.has_one_of<Ts...>(ID); }
 
 
-    private:
+private:
 
-        // A reference to the scene this entity belongs to.
-        Scene& scene;
+    // A reference to the scene this entity belongs to.
+    QKScene& scene;
 
-        // The unique identifier for this entity.
-        entt::entity ID;
+    // The unique identifier for this entity.
+    entt::entity ID;
 
 
-    };
+};
 
-    // Auxillary function to determine if a generic entity is another entity type
-    template <typename ent_t>
-    inline bool is(const Entity& entity) {
-        return entity.has<typename ent_t::tag>();
-    }
+// QKActor
+struct QKActor : public QKEntity {
+    QKActor(QKScene& scene): QKEntity(scene) { add<QKActorChildren>(); }
 
+    //QKActor(QKScene& scene, entt::entity existing_entity) : QKEntity(scene, existing_entity) {}
+        
+    //  QKActor get_child(size_t index) {
+    //      return QKActor{ get_scene(), get_ID() };
+    //  }
+};
+
+// Auxillary function to determine if a generic entity is another entity type
+template <typename ent_t>
+inline bool is(const QKEntity& entity) {
+    return entity.has<typename ent_t::tag>();
 }
-
-using Entity = Quakaster::base::Entity;
