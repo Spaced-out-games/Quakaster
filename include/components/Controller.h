866c13103bcc57ff0280b83166cefae42fde8b44
Devin Frost <devin.frost4311@gmail.com>
2025-03-14 12:30:35 -0400

    Controller was missing responses to mouse clicks. Fixed

--- include/components/Controller.h
+++ include/components/Controller.h
@@ -13,20 +13,24 @@ struct Controller {
 		std::function<void(KeyPressEvent)> on_key_press_fn = [](KeyPressEvent) {},
 		std::function<void(KeyHoldEvent)> on_key_hold_fn = [](KeyHoldEvent) {},
 		std::function<void(KeyReleaseEvent)> on_key_release_fn = [](KeyReleaseEvent) {},
-		std::function<void(MouseMoveEvent)> on_mouse_move_fn = [](MouseMoveEvent) {}
+		std::function<void(MouseMoveEvent)> on_mouse_move_fn = [](MouseMoveEvent) {},
+		std::function<void(MouseClickEvent)> on_mouse_click_fn = [](MouseClickEvent) {}
+
 	) :
 		on_key_press(on_key_press_fn),
 		on_key_hold(on_key_hold_fn),
 		on_key_release(on_key_release_fn),
-		on_mouse_move(on_mouse_move_fn)
+		on_mouse_move(on_mouse_move_fn),
+		on_mouse_click(on_mouse_click_fn)
 	{}
 
 	protected:
-		EventListener<KeyPressEvent> on_key_press;
-		EventListener<KeyHoldEvent> on_key_hold;
-		EventListener<KeyReleaseEvent> on_key_release;
-		EventListener<MouseMoveEvent> on_mouse_move;
-
+		QKEventListener<KeyPressEvent> on_key_press;
+		QKEventListener<KeyHoldEvent> on_key_hold;
+		QKEventListener<KeyReleaseEvent> on_key_release;
+		QKEventListener<MouseMoveEvent> on_mouse_move;
+		QKEventListener<MouseClickEvent> on_mouse_click;
+		
 	//private:
 
 
