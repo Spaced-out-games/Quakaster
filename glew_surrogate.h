26635cdbfb5df26be9f0f04e11f2cbbc6a9ef97d
Devin Frost <devin.frost4311@gmail.com>
2025-03-14 13:00:17 -0400

    Not includes glm types, too. Might want tot decouple this

--- glew_surrogate.h
+++ glew_surrogate.h
@@ -1,2 +1,8 @@
 #pragma once
 #include <GL/glew.h>
+#include <include/thirdparty/glm/glm.hpp> // For glm::vec3
+#include <include/thirdparty/glm/gtc/quaternion.hpp> // For glm::quat
+#include <include/thirdparty/glm/gtc/matrix_transform.hpp>
+#include <include/thirdparty/glm/gtc/type_ptr.hpp>
+#include <include/thirdparty/glm/mat4x4.hpp> // For glm::mat4
+
