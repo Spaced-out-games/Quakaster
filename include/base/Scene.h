a7407f7e4c8fe6c92a84e423dd0a8ae9356c48f1
Devin Frost <devin.frost4311@gmail.com>
2025-03-14 12:22:31 -0400

    New naming convention. I love it

--- include/base/Scene.h
+++ include/base/Scene.h
@@ -4,68 +4,63 @@
 #include <include/base/Component.h>
 
 
-namespace Quakaster::base {
+// TODO: Move functionality from dispatcher into its own wrapped type, then make it a component of QKScene.
+// Since almost all of them use event_handler& already, per the custom #define in entt, it should be pretty easy to simply move it over there
+// Once that's done, you might want to double, triple check that every component properly follows conventions
+// Once THAT's done, move to defining entity types and go around, fixing that shit. 
+struct QKScene
+{
 
-	// TODO: Move functionality from dispatcher into its own wrapped type, then make it a component of Scene.
-	// Since almost all of them use event_handler& already, per the custom #define in entt, it should be pretty easy to simply move it over there
-	// Once that's done, you might want to double, triple check that every component properly follows conventions
-	// Once THAT's done, move to defining entity types and go around, fixing that shit. 
-	struct Scene
-	{
+	inline entt::entity create() { return registry.create(); }
 
-
-
-		template <typename T, typename ...Args>
-		inline T& add_component(entt::entity entity, Args&&... args) {
-			//static_assert(std::is_base_of_v<Quakaster::base::Component, T>, "T must be a component type.");
-			return registry.emplace<T>(entity, std::forward<Args>(args)...);
-		}
+	inline void destroy(entt::entity entity) { registry.destroy(entity); }
 
 
+	template <typename T, typename ...Args>
+	inline T& add(entt::entity entity, Args&&... args) {
+		return registry.emplace<T>(entity, std::forward<Args>(args)...);
+	}
 
 
-		template <typename T>
-		inline T& get_component(entt::entity entity) {
-			return registry.get<T>(entity);
-		}
-
-		template <typename T>
-		inline T* try_get_component(entt::entity entity) {
-			return registry.try_get<T>(entity);
-		}
-
-		template <typename T>
-		inline bool has(entt::entity entity) {
-			return registry.all_of<T>(entity);
-		}
-
-		template <typename ...Ts>
-		inline bool has_all_of(entt::entity entity) {
-			return registry.all_of<Ts...>(entity);
-		}
-		template <typename ...Ts>
-		inline bool has_one_of(entt::entity entity) {
-			return registry.any_of<Ts...>(entity);
-		}
-		template <typename T>
-		inline void remove_component(entt::entity entity) {
-			registry.remove<T>(entity);
-		}
-
-		entt::registry& get_registry() { return registry; }
-
-		template <typename... Ts>
-		inline auto view() {
-			return registry.view<Ts...>();
-		}
-
-		private:
-			friend Entity;
-			entt::registry registry;
-
-	};
 
 
-}
+	template <typename T>
+	inline T& get(entt::entity entity) {
+		return registry.get<T>(entity);
+	}
 
-using Scene = Quakaster::base::Scene;
+	template <typename T>
+	inline T* try_get(entt::entity entity) {
+		return registry.try_get<T>(entity);
+	}
+
+	template <typename T>
+	inline bool has(entt::entity entity) {
+		return registry.all_of<T>(entity);
+	}
+
+	template <typename ...Ts>
+	inline bool has_all_of(entt::entity entity) {
+		return registry.all_of<Ts...>(entity);
+	}
+	template <typename ...Ts>
+	inline bool has_one_of(entt::entity entity) {
+		return registry.any_of<Ts...>(entity);
+	}
+	template <typename T>
+	inline void remove_component(entt::entity entity) {
+		registry.remove<T>(entity);
+	}
+
+	entt::registry& get_registry() { return registry; }
+
+	template <typename... Ts>
+	inline auto view() {
+		return registry.view<Ts...>();
+	}
+
+	private:
+		friend QKEntity;
+		entt::registry registry;
+
+};
