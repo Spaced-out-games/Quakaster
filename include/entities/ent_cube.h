38a9b03b842646a0c1730297b94d87f3ad6a50fb
Devin Frost <devin.frost4311@gmail.com>
2025-03-14 12:38:30 -0400

    New naming convention. I love it Updated the system initializer Cube now can be any size

--- include/entities/ent_cube.h
+++ include/entities/ent_cube.h
@@ -2,34 +2,29 @@
 #include <include/base/Entity.h>
 #include <include/base/Scene.h>
 #include <include/resources/res_texture.h>
-#include <include/resources/mesh.h>
-//#include <include/thirdparty/glm/vec3.hpp>
+#include <include/components/AABB.h>
+#include <include/resources/mesh_manager.h>
 
 
 
-//struct Scale : glm::vec3 {};
-// cube entity
+struct ent_cube : QKEntity {
+	//static mesh cube_mesh; // this is legal because it's static
 
-namespace Quakaster::entities {
-	struct ent_cube : Entity {
-		//static mesh cube_mesh; // this is legal because it's static
+	struct tag : Tag {};
 
-		struct tag : public Tag {};
+	struct system : ISystem {
+		// just initializes the cube mesh
+		void init(QKScene&) {
 
-		//static void init()
-
-		ent_cube(Scene& scene) : Entity(scene) {
-
-			// Initialize the vertices and indices of the cube
 			std::vector<default_vertex_t> vertices = {
-				{{0.0f, 0.0f, 0.0f}},  // Vertex 0: Bottom-left-back
-				{{100.0f, 0.0f, 0.0f}},  // Vertex 1: Bottom-right-back
-				{{100.0f, 1.0f, 0.0f}},  // Vertex 2: Top-right-back
-				{{0.0f, 1.0f, 0.0f}},  // Vertex 3: Top-left-back
-				{{0.0f, 0.0f, 100.0f}},  // Vertex 4: Bottom-left-front
-				{{100.0f, 0.0f, 100.0f}},  // Vertex 5: Bottom-right-front
-				{{100.0f, 1.0f, 100.0f}},  // Vertex 6: Top-right-front
-				{{0.0f, 1.0f, 100.0f}},  // Vertex 7: Top-left-front
+				{{-0.5f, -0.5f, -0.5f}},  // Vertex 0: Bottom-left-back
+				{{ 0.5f, -0.5f, -0.5f}},  // Vertex 1: Bottom-right-back
+				{{ 0.5f,  0.5f, -0.5f}},  // Vertex 2: Top-right-back
+				{{-0.5f,  0.5f, -0.5f}},  // Vertex 3: Top-left-back
+				{{-0.5f, -0.5f,  0.5f}},  // Vertex 4: Bottom-left-front
+				{{ 0.5f, -0.5f,  0.5f}},  // Vertex 5: Bottom-right-front
+				{{ 0.5f,  0.5f,  0.5f}},  // Vertex 6: Top-right-front
+				{{-0.5f,  0.5f,  0.5f}}   // Vertex 7: Top-left-front
 			};
 			std::vector<uint32_t> indices = {
 				// Back face
@@ -51,54 +46,56 @@ namespace Quakaster::entities {
 				0, 1, 5,  // Flipped 1 and 5
 				0, 5, 4   // Flipped 5 and 4
 			};
-			add_component<ent_cube::tag>();
-			//add_component<Mesh>(
-		//		vertices,
-		//		indices,
-		//		"default_shader",
-		//		"resources/shaders/default.vert",
-		//		"resources/shaders/default.frag"
-		//		);
-		//	add_component<Transform>();
-		//	add_component<Texture>("resources/images/atlas.png");
+
+
+
+			MeshManager::generate_mesh(
+				"cube",
+				vertices,
+				indices,
+				"default_shader",
+				"resources/shaders/default.vert",
+				"resources/shaders/default.frag"
+			);
 		}
-
-		static inline ent_cube& cast(Entity& ent) {
-			// Assuming that `ent` is guaranteed to be of type `ent_cube`.
-			return static_cast<ent_cube&>(ent);
-		}
-
-
-
-		static inline ent_cube* try_cast(Entity& entity) {
-			return dynamic_cast<ent_cube*>(&entity);
-		}
-
-
-		// getters, setters, actions
-		inline Transform& transform() { return get_component<Transform>(); }
-		//inline Mesh& mesh() { return get_component<Mesh>(); }
-		inline Texture& texture() { return get_component<Texture>(); }
-
-
-		// Operations forwarded from Transform
-
-		inline void move(const glm::vec3& delta_pos) { transform().move(delta_pos); }
-		inline void move_to(const glm::vec3& new_pos) { transform().move_to(new_pos); }
-		inline glm::vec3 get_forward_vector() { return transform().get_forward_vector(); }
-		inline glm::vec3 get_up_vector() { return transform().get_up_vector(); }
-		inline glm::vec3 get_right_vector() { return transform().get_right_vector(); }
-		inline glm::vec3 get_euler_angles() { return transform().get_euler_angles(); }
-		inline glm::mat4 get_matrix() { return transform().get_matrix(); }
-		inline void look_at(const glm::vec3& target) { transform().look_at(target); }
-
-		// Additional operation to look at the target
-		inline void look_at(Entity& target) {
-			if (target.has<Transform>()) transform().look_at(target.get_component<Transform>().position);
-
-		}
-
+		void tick(QKScene&) {}
+		void destroy(QKScene&) {}
 	};
-}
 
-using ent_cube = Quakaster::entities::ent_cube;
+
+	//static void init()
+
+	ent_cube(QKScene& scene, glm::vec3 position = { 0,0,0 }, glm::vec3 scale = { 1,1,1 }) : QKEntity(scene) {
+		add<Transform>();
+		get<Transform>().position = position;
+
+		add<MeshInstance>("cube");
+		get<MeshInstance>().submit(Matrix{}.scale(scale));
+			
+		add<AABB>(scale.x, scale.y, scale.z);
+		add<ent_cube::tag>();
+
+	}
+
+	static inline ent_cube& cast(QKEntity& ent) {
+		// Assuming that `ent` is guaranteed to be of type `ent_cube`.
+		return static_cast<ent_cube&>(ent);
+	}
+
+
+
+	static inline ent_cube* try_cast(QKEntity& entity) {
+		return dynamic_cast<ent_cube*>(&entity);
+	}
+
+
+	// getters, setters, actions
+	inline Transform& transform() { return get<Transform>(); }
+	inline MeshInstance& mesh() { return get<MeshInstance>(); }
+	inline AABB& aabb() { return get<AABB>(); }
+
+	inline void rescale(glm::vec3 scale) {
+		// mesh().submit(glm::scale)
+	}
+
+};
