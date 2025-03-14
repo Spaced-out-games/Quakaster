e5667bfb293bfba0773b65c57c14c27e98544dcf
Devin Frost <devin.frost4311@gmail.com>
2025-03-14 12:26:04 -0400

    Removed 'using' declaration Tag moved out of namespace

--- include/base/Tag.h
+++ include/base/Tag.h
@@ -1,17 +1,11 @@
 #pragma once
 
-namespace Quakaster::base {
 
-	/// <summary>
-	/// Tag interface for entities for use within EnTT. Make sure 
-	/// </summary>
-	struct Tag {
-		// These are here so that EnTT doesn't optimize tags away. Do with these bits as you wish
-		uint8_t buffer;
+/// <summary>
+/// Tag interface for entities for use within EnTT. Make sure 
+/// </summary>
+struct Tag {
+	// These are here so that EnTT doesn't optimize tags away. Do with these bits as you wish
+	uint8_t buffer;
 
-	};
-
-}
-
-
-using Tag = Quakaster::base::Tag;
+};
