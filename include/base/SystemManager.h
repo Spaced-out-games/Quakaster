cac28570f8a3024ff9f2186fb7dc6e6bb5274d42
Devin Frost <devin.frost4311@gmail.com>
2025-03-14 12:25:22 -0400

    add_system() now returns a pointer to the added system

--- include/base/SystemManager.h
+++ include/base/SystemManager.h
@@ -21,9 +21,9 @@ public:
 
     // Add a new system
     template <typename system_t, typename... Args>
-    void add_system(Args&&... args) {
+    ISystem* add_system(Args&&... args) {
         systems.push_back(new system_t(std::forward<Args>(args)...)); // Allocate memory for the new system
-        //systems.back()->init(); // Call init on the newly added system
+        return systems.at(systems.size() - 1);
     }
 
     // Destructor to clean up allocated systems
@@ -35,15 +35,15 @@ public:
 protected:
     friend class Client;
     friend class Server;
-    inline void init_system(int index, Scene& scene) const {
+    inline void init_system(int index, QKScene& scene) const {
         systems[index]->init(scene);
     }
 
-    inline void tick_system(int index, Scene& scene) const {
+    inline void tick_system(int index, QKScene& scene) const {
         systems[index]->tick(scene);
     }
 
-    inline void destroy_system(int index, Scene& scene) const {
+    inline void destroy_system(int index, QKScene& scene) const {
 
     }
 private:
