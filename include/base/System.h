dc028d430976ebc0750619faf9c4312f550ab7b1
Devin Frost <devin.frost4311@gmail.com>
2025-03-14 12:22:44 -0400

    New naming convention. I love it

--- include/base/System.h
+++ include/base/System.h
@@ -1,17 +1,14 @@
 #pragma once
 #include <include/base/Scene.h>
 
-namespace Quakaster::base {
-	/// <summary>
-	/// Represents a single broad-phase operation on entities. Useful for modularization. They ARE allowed to store internal state...
 
-	/// </summary>
-	struct ISystem {
-		virtual void init(Scene& scene) = 0;
-		virtual void tick(Scene& scene) = 0;
-		virtual void destroy(Scene& scene) = 0;
-	};
-}
+/// <summary>
+/// Represents a single broad-phase operation on entities. Useful for modularization. They ARE allowed to store internal state...
 
+/// </summary>
+struct ISystem {
+	virtual void init(QKScene& scene) = 0;
+	virtual void tick(QKScene& scene) = 0;
+	virtual void destroy(QKScene& scene) = 0;
+};
 
-using ISystem = Quakaster::base::ISystem;
