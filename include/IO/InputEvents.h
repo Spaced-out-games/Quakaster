9c41ff81db56150795c521b902e1c4552f75e0db
Devin Frost <devin.frost4311@gmail.com>
2025-03-14 12:42:52 -0400

    New naming convention. I love it

--- include/IO/InputEvents.h
+++ include/IO/InputEvents.h
@@ -6,16 +6,16 @@
 
 
 // Triggered when a key is pressed
-struct KeyPressEvent: Event { SDL_Keycode code; Uint16 mod; KeyPressEvent(SDL_Keycode code, Uint16 mod = KMOD_NONE) : code(code), mod(mod) {} };
+struct KeyPressEvent: QKEvent { SDL_Keycode code; Uint16 mod; KeyPressEvent(SDL_Keycode code, Uint16 mod = KMOD_NONE) : code(code), mod(mod) {} };
 
 // Triggered when a key is held down
-struct KeyHoldEvent: Event { SDL_Keycode code; Uint32 duration; KeyHoldEvent(SDL_Keycode code) : code(code), duration(duration) {} };
+struct KeyHoldEvent: QKEvent { SDL_Keycode code; Uint32 duration; KeyHoldEvent(SDL_Keycode code) : code(code), duration(duration) {} };
 
 // Triggered when a key is released
-struct KeyReleaseEvent: Event { SDL_Keycode code; Uint16 mod; KeyReleaseEvent(SDL_Keycode code, Uint16 mod = KMOD_NONE) : code(code), mod(mod) {} };
+struct KeyReleaseEvent: QKEvent { SDL_Keycode code; Uint16 mod; KeyReleaseEvent(SDL_Keycode code, Uint16 mod = KMOD_NONE) : code(code), mod(mod) {} };
 
 // Triggered when a mouse button is pressed
-struct MouseClickEvent: Event {
+struct MouseClickEvent: QKEvent {
 	Uint8 button;
 	bool down;
 	Sint32 x;
@@ -24,10 +24,10 @@ struct MouseClickEvent: Event {
 };
 
 // Triggered when a mouse button is released
-struct MouseReleaseEvent: Event { Uint8 button; Uint16 mod; MouseReleaseEvent(Uint8 button, Uint16 mod = KMOD_NONE) : button(button), mod(mod) {} };
+struct MouseReleaseEvent: QKEvent { Uint8 button; Uint16 mod; MouseReleaseEvent(Uint8 button, Uint16 mod = KMOD_NONE) : button(button), mod(mod) {} };
 
 // Triggered when the mouse is moved
-struct MouseMoveEvent : Event {
+struct MouseMoveEvent : QKEvent {
 	glm::ivec2 current_position;
 	glm::ivec2 relative_position;
 	Uint16 mod;
@@ -36,10 +36,10 @@ struct MouseMoveEvent : Event {
 };
 
 // Triggered when the mouse wheel is scrolled
-struct MouseScrollEvent : Event { int x, y; Uint16 mod; MouseScrollEvent(int x, int y, Uint16 mod = KMOD_NONE) : x(x), y(y), mod(mod) {} };
+struct MouseScrollEvent : QKEvent { int x, y; Uint16 mod; MouseScrollEvent(int x, int y, Uint16 mod = KMOD_NONE) : x(x), y(y), mod(mod) {} };
 
 // Triggered when the window state changes
-struct WindowEvent : Event {
+struct WindowEvent : QKEvent {
 	Uint8 event_type;   // Type of window event (e.g., SDL_WINDOWEVENT_RESIZED, SDL_WINDOWEVENT_FOCUS_GAINED, etc.)
 	int data1;          // Data associated with the event (e.g., width for resize)
 	int data2;          // Additional data (e.g., height for resize)
@@ -48,10 +48,10 @@ struct WindowEvent : Event {
 		: event_type(event_type), data1(data1), data2(data2) {}
 };
 
-struct PauseEvent : Event {
+struct PauseEvent : QKEvent {
 	bool new_pause;
 };
 
-struct TickEvent : Event {
+struct TickEvent : QKEvent {
 	float tick_interval;
 };
